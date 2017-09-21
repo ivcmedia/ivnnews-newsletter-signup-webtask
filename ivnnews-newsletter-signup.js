721c2119c79ba3a17c7ff29b742beb2bfbaf253b
M. George Hansen <mgeorgehansen@users.noreply.github.com>
2017-09-21 13:19:48 -0700

    Allow unauthenticated requests

--- ivnnews-newsletter-signup.js
+++ ivnnews-newsletter-signup.js
@@ -13,25 +13,30 @@ function parseFormdata(req) {
   });
 }
 
-module.exports = function(context, cb) {
-  if (context.body == null) {
-    return cb('empty body');
-  }
-  if (context.body.email_address == null) {
-    return cb('body must have an email_address field');
-  }
-  var mailchimp = new Mailchimp(context.secrets.mailchimpApiKey);
-  var path = `/lists/${context.meta.mailchimpListId}/members`;
-  console.log(`making POST request to ${path}`);
-  return mailchimp.post(`/lists/${context.meta.mailchimpListId}/members`, {
-      email_address: context.body.email_address,
-      status: 'subscribed',
+module.exports = function(context, req, res) {
+  parseFormdata(req)
+    .then(function(data) {
+      if (data.fields.email_address == null) {
+        return cb('body must have an email_address field');
+      }
+      var mailchimp = new Mailchimp(context.secrets.mailchimpApiKey);
+      var path = `/lists/${context.meta.mailchimpListId}/members`;
+      console.log(`making POST request to ${path}`);
+      return mailchimp.post(`/lists/${context.meta.mailchimpListId}/members`, {
+        email_address: context.body.email_address,
+        status: 'subscribed',
+      });    
     })
     .then(function(res) {
       console.log('success!');
-      cb(null);
+      res.writeHead(200);
+      res.end();
     })
     .catch(function(error) {
-      cb({type: error.type, title: error.title, detail: error.detail, instance: error.instance});
+      res.writeHead(error.status, {
+        'Content-Type': 'application/json',
+      });
+      var json = JSON.stringify({type: error.type, title: error.title, detail: error.detail, instance: error.instance});
+      res.end(json);
     });
 };
