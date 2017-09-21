6fcd888a8183385226a2de8a50b9a9dd49f12864
M. George Hansen <mgeorgehansen@users.noreply.github.com>
2017-09-21 13:53:59 -0700

    Updating ivnnews-newsletter-signup.js

--- ivnnews-newsletter-signup.js
+++ ivnnews-newsletter-signup.js
@@ -1,48 +1,45 @@
-var Mailchimp = require('mailchimp-api-v3');
-var parseFormdata_ = require('parse-formdata');
+var Mailchimp  = require('mailchimp-api-v3');
+var express    = require('express');
+var Webtask    = require('webtask-tools');
+var bodyParser = require('body-parser');
 
-function parseFormdata(req) {
-  return new Promise(function(resolve, reject) {
-    parseFormdata_(req, function(err, data) {
-      if (err != null) {
-        console.log('parseFormdata error: %o', err);
-        return reject({status: 400, message: err.message || 'unknown error'});
-      }
-      return resolve(data);
-    });
-  });
-}
-
-module.exports = function(context, req, res) {
-  parseFormdata(req)
-    .then(function(data) {
-      if (data.fields.email_address == null) {
-        return cb('body must have an email_address field');
-      }
-      var mailchimp = new Mailchimp(context.secrets.mailchimpApiKey);
-      var path = `/lists/${context.meta.mailchimpListId}/members`;
-      console.log(`making POST request to ${path}`);
-      return mailchimp.post(`/lists/${context.meta.mailchimpListId}/members`, {
-        email_address: context.body.email_address,
-        status: 'subscribed',
-      }).catch(function(err) {
-        console.log('mailchimp api call error: %o', err);
-        return Promise.reject({status: err.status || 500, message: err.message});
-      });    
-    })
-    .then(function(res) {
+var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.get('/', function (req, res, next) {
+  if (req.body.email_address == null) {
+    var error = new Error('body must have an email_address field');
+    error.status = 400;
+    return next(error);
+  }
+  var mailchimp = new Mailchimp(context.secrets.mailchimpApiKey);
+  var path = `/lists/${context.meta.mailchimpListId}/members`;
+  console.info(`making POST request to ${path}`);
+  return mailchimp.post(`/lists/${context.meta.mailchimpListId}/members`, {
+    email_address: context.body.email_address,
+    status: 'subscribed',
+  }).catch(function(err) {
+    console.error('mailchimp api call error: %o', err);
+    return Promise.reject(err);
+  })
+  .then(function(res) {
       console.log('success!');
-      res.writeHead(200);
-      res.end();
+      res.status(200).end();
     })
-    .catch(function(error) {
-      res.writeHead(error.status, {
-        'Content-Type': 'application/json',
-      });
-      var json = JSON.stringify({
-        status: error.status,
-        message: error.message,
-      });
-      res.end(json);
+  .catch(function(error) {
+    var json = JSON.stringify({
+      status: error.status,
+      message: error.message,
     });
-};
+    res.send(json);
+  });
+});
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  var status = err.status || 500;
+  res.status(status);
+  res.send({status: status, error: err});
+});
+
+module.exports = Webtask.fromExpress(app);
