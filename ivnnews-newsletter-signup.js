f00c90c98a34a7a1eebda6de4ced6b1b40a15ca7
M. George Hansen <mgeorgehansen@users.noreply.github.com>
2017-10-09 16:00:50 -0700

    Support wildcard CORS request

--- ivnnews-newsletter-signup.js
+++ ivnnews-newsletter-signup.js
@@ -1,16 +1,16 @@
-var Mailchimp  = require('mailchimp-api-v3');
-var express    = require('express');
-var Webtask    = require('webtask-tools');
+var Mailchimp = require('mailchimp-api-v3');
+var express = require('express');
+var Webtask = require('webtask-tools');
 var bodyParser = require('body-parser');
-var cors       = require('cors');
-var morgan     = require('morgan');
+var cors = require('cors');
+var morgan = require('morgan');
 
 var corsOptions = function(req, cb) {
   var allowedOrigin = req.webtaskContext.meta.corsAllowedOrigins || '*';
   console.log('found allowed origin: ', allowedOrigin);
   cb(null, {
     origin: function(origin, originCb) {
-      if (allowedOrigin === origin) {
+      if (allowedOrigin === '*' || allowedOrigin === origin) {
         return originCb(null, true);
       }
       originCb(new Error('origin not allowed'), false);
@@ -24,10 +24,12 @@ var corsOptions = function(req, cb) {
 var app = express();
 app.use(morgan('combined'));
 app.use(cors(corsOptions));
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-app.post('/', function (req, res, next) {
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.post('/', function(req, res, next) {
   if (req.body.email_address == null) {
     var error = new Error('body must have an email_address field');
     error.status = 400;
@@ -44,33 +46,35 @@ app.post('/', function (req, res, next) {
   var mailchimp = new Mailchimp(secrets.mailchimpApiKey);
   var path = `/lists/${meta.mailchimpListId}/members`;
   console.info(`making POST request to ${path}`);
-  return mailchimp.post(`/lists/${meta.mailchimpListId}/members`, {
-    email_address: emailAddress,
-    status: 'subscribed',
-    interests: {
-      [meta.mailchimpInterestId]: true,
-    },
-  }).catch(function(err) {
-    console.error('mailchimp api call error: %o', err);
-    if (err.status === 400) {
-      return Promise.resolve();
-    }
-    return Promise.reject(err);
-  })
-  .then(function() {
+  return mailchimp
+    .post(`/lists/${meta.mailchimpListId}/members`, {
+      email_address: emailAddress,
+      status: 'subscribed',
+      interests: {
+        [meta.mailchimpInterestId]: true,
+      },
+    })
+    .catch(function(err) {
+      console.error('mailchimp api call error: %o', err);
+      if (err.status === 400) {
+        return Promise.resolve();
+      }
+      return Promise.reject(err);
+    })
+    .then(function() {
       console.log('success!');
       res.status(200).end();
-  })
-  .catch(function(err) {
-    next(err);
-  });
+    })
+    .catch(function(err) {
+      next(err);
+    });
 });
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   var status = err.status || 500;
   res.status(status);
   var message = status === 500 ? 'Interal error' : err.message;
-  res.send({status: status, error: message});
+  res.send({ status: status, error: message });
 });
 
 module.exports = Webtask.fromExpress(app);
